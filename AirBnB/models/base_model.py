96c02dfeb3bfa5d5610d423166ccb0a2d81678a7
Leidysalda <leidysalda1@gmail.com>
2020-02-14 16:23:20 +0000

    Add files

---
+++ AirBnB/models/base_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin env python3
+"""Defines all common attributes/methods for other classes
+"""
+
+class BaseModel:
+    """Class base of project
+    """
+
+    def __init__(self, id=None):
+        """Constructor"""
